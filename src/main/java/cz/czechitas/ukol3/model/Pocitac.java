789787ab07de452ea9b6b4f9924b2bf50649a7e5
Olinka <olga.titzenthaler@gmail.com>
2024-02-25 13:52:21 +0100

    - pridany metody vytvorSouborOvelikosti a vymazSouborOvelikosti

--- src/main/java/cz/czechitas/ukol3/model/Pocitac.java
+++ src/main/java/cz/czechitas/ukol3/model/Pocitac.java
@@ -6,15 +6,10 @@ public class Pocitac {
     public boolean procesor = false;
     public boolean pamet = false;
     public boolean disk = false;
-
     public Disk pevnyDisk;
 
     public Pocitac() {
         this.pevnyDisk = new Disk();
-    }
-
-    public Disk getPevnyDisk() {
-        return pevnyDisk;
     }
 
     public void setPevnyDisk(Disk pevnyDisk) {
@@ -82,13 +77,12 @@ public class Pocitac {
 
     public void vytvorSouborOvelikosti(long velikostSouboru) {
         long vyuzitelneMisto = pevnyDisk.getKapacita() - pevnyDisk.getVyuziteMisto();
-        if ((velikostSouboru < vyuzitelneMisto)&&(0<velikostSouboru)) {
+        if ((velikostSouboru < vyuzitelneMisto) && (0 < velikostSouboru)) {
             pevnyDisk.setKapacita(pevnyDisk.getKapacita() - velikostSouboru);
             pevnyDisk.setVyuziteMisto(pevnyDisk.getVyuziteMisto() + velikostSouboru);
-            System.out.println("Soubor o velikosti " + velikostSouboru + " bajtu byl ulozeny na disk." + "Vyuzitelna kapacita disku je pouze " + pevnyDisk.getKapacita()+ " bajtů.");
-        }
-        else {
-            System.out.println("Soubor nelze ulozit. Byla prekrocena kapacita disku. Zbyvajici kapacita disku je pouze " + pevnyDisk.getKapacita()+ " bajtů.");
+            System.out.println("Soubor o velikosti " + velikostSouboru + " bajtu byl ulozeny na disk." + "Vyuzitelna kapacita disku je pouze " + pevnyDisk.getKapacita() + " bajtů.");
+        } else {
+            System.out.println("Soubor nelze ulozit. Byla prekrocena kapacita disku. Zbyvajici kapacita disku je pouze " + pevnyDisk.getKapacita() + " bajtů.");
         }
     }
 
@@ -98,8 +92,7 @@ public class Pocitac {
             pevnyDisk.setKapacita(pevnyDisk.getKapacita() + velikostSouboru);
             pevnyDisk.setVyuziteMisto(pevnyDisk.getVyuziteMisto() - velikostSouboru);
             System.out.println("Soubor o velikosti " + velikostSouboru + " bajtu byl smazany z disku." + "Vyuztelna kapacita disku je " + pevnyDisk.getKapacita() + " bajtů.");
-        }
-        else {
+        } else {
             System.out.println("Soubor o velikosti " + velikostSouboru + " je vetsi, nez vyuzite misto disku. Vyuzitelna kapacita disku je " + pevnyDisk.getVyuziteMisto() + " bajtů.");
         }
     }
